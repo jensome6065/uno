47bea8d4018be31570b0f7c29e9d0ab88160ea48
Jennifer Ye <jensome6065@gmail.com>
2024-10-16 17:00:14 -0400

    added buildDeck function

--- uno.py
+++ uno.py
@@ -1,6 +1,20 @@
 # python3 -m pip install PySimpleGUI
 import PySimpleGUI as sg
 
+def buildDeck():
+    deck = []
+    colors = ["R", "Y", "G", "B"]
+    values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "+2", "skip", "reverse"]
+    wilds = ["W", "W +4"]
+    for c in colors: 
+        for v in values:
+            cardVal = "{} {}".format(c, v)
+            deck.append(cardVal)
+            if v != 0:
+                deck.append(cardVal)
+    deck.extend(wilds)
+    return deck
+
 layout = [[sg.Text("welcome to our UNO game!", font = ('Helvetica', 40), justification = 'center')],
           [sg.Button("start", font = ('Helvetica', 20), size = (10, 2))]]
 
@@ -12,6 +26,5 @@ while True:
         break
     elif event == "start":
         sg.popup("game is starting...")
-        break
 
 window.close()
