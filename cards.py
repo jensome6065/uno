da7e9fa37350694375f370ae940ef0e58216127b
Jennifer Ye <jensome6065@gmail.com>
2024-12-09 11:48:42 -0500

    handled case where deck is empty

--- cards.py
+++ cards.py
@@ -36,5 +36,15 @@ class Deck:
     def shuffle(self):
         random.shuffle(self.cards)
 
-    def draw_card(self):
+    def draw_card(self, discard_stack=None):
+        if len(self.cards) == 0 and discard_stack is not None:
+            self.reshuffle_from_discard(discard_stack)
         return self.cards.pop()
+    
+    def reshuffle_from_discard(self, discard_stack):
+        if len(self.cards) == 0:
+            top_card = discard_stack.pop() 
+            self.cards = discard_stack[:] 
+            discard_stack.clear()
+            discard_stack.append(top_card)
+            self.shuffle()
