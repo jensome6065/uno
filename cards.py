f7490789c3727e70cfd1c622dc6fd0842c0fb7bd
Jennifer Ye <jensome6065@gmail.com>
2024-12-08 21:12:53 -0500

    updated card and deck classes

--- cards.py
+++ cards.py
@@ -1,22 +1,5 @@
 import random
 
-class Deck:
-    def __init__(self):
-        self.cards = self.build_deck()
-        random.shuffle(self.cards)
-
-    def build_deck(self):
-        colors = ["Red", "Yellow", "Green", "Blue"]
-        values = [str(i) for i in range(10)] + ["Skip", "Reverse", "+2"]
-        deck = [Card(color, value) for color in colors for value in values]
-        deck += deck  # Two of each non-zero card
-        deck += [Card("Wild", "Wild"), Card("Wild", "Wild +4")] * 4  # Wild cards
-        return deck
-
-    def draw(self):
-        return self.cards.pop() if self.cards else None
-
-
 class Card:
     def __init__(self, color, value):
         self.color = color
@@ -25,10 +8,31 @@ class Card:
     def __repr__(self):
         return f"{self.color} {self.value}"
 
-    def is_playable_on(self, top_card):
-        return (
-            self.color == "Wild" or
-            top_card.color == "Wild" or
-            self.color == top_card.color or
-            self.value == top_card.value
-        )
+class Deck:
+    def __init__(self):
+        self.colors = ['Red', 'Yellow', 'Green', 'Blue']
+        self.values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Skip', 'Reverse', '+2']
+        self.wild_cards = ['Wild Color', 'Wild +4', 'Wild Number']  
+        self.cards = []
+        self.create_deck()
+        self.shuffle()
+
+    def create_deck(self):
+        for color in self.colors:
+            for value in self.values:
+                if value != '0':
+                    self.cards.append(Card(color, value))
+                    self.cards.append(Card(color, value))
+                else:
+                    self.cards.append(Card(color, value))
+        
+        for _ in range(4):
+            self.cards.append(Card('Wild', 'Color'))
+            self.cards.append(Card('Wild', '+4'))
+            self.cards.append(Card('Wild', 'Number')) 
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def draw_card(self):
+        return self.cards.pop()
