d84d09caf28812b6ed2db9ac0118403f36f81847
Jennifer Ye <jensome6065@gmail.com>
2024-12-08 21:13:03 -0500

    updated player class

--- player.py
+++ player.py
@@ -3,12 +3,17 @@ class Player:
         self.name = name
         self.hand = []
 
-    def draw_card(self, deck):
-        card = deck.draw()
-        if card:
+    def draw(self, deck, num_cards=1):
+        for _ in range(num_cards):
+            card = deck.draw_card()
             self.hand.append(card)
-        return card
 
     def play_card(self, card):
         self.hand.remove(card)
         return card
+
+    def has_won(self):
+        return len(self.hand) == 0
+
+    def __repr__(self):
+        return f"{self.name} has {len(self.hand)} cards"
